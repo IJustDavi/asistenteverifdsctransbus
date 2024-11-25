bf389e781449c083f831e84141c8ac1b7c26f207
IJustDavi <87678581+IJustDavi@users.noreply.github.com>
2024-11-25 11:46:11 -0500

    Create index.php

---
+++ src/src/index.php
@@ -0,0 +1,141 @@
+<html><head>
+<title>.: Aplicativos TRANSBUS :.</title>
+<!--Utilizando como inspiración plantillas antiguas de Microsoft actualmente archivadas en Wayback Machine. Derechos de autor a sus respectivos autores-->
+<!--STYLE_START-->
+<link rel="STYLESHEET" href="https://web.archive.org/web/20000619204746cs_/http://support.microsoft.com/support/include/style.css" type="text/css">
+<!--STYLE_END-->
+
+<!--META_START-->
+<meta name="Premium" content="Aplicativo Verif Discord TransBus">
+<meta http-equiv="Content-Type" content="text/html; charset=iso8859-1">
+<meta name="ms.locale" content="EN-US">
+<meta name="robots" content="noindex">
+<!--META_END-->
+</head>
+
+<body topmargin="0" rightmargin="0" marginwidth="0" marginheight="0" leftmargin="0" bgcolor="#FFFFFF">
+ 
+<!-- #include virtual="/support/include/getparms.inc" -->
+<!-- #include virtual="/library/include/server.inc" -->
+
+<!--TOOLBAR_START-->
+<a name="TOP"></a>
+
+<!-- Start: ToolBar for down-level browsers-->
+<span id="TBDownLevelDiv">
+<table width="100%" cellpadding="0" cellspacing="0" border="0" bgcolor="#FFFFFF">
+<tbody><tr>
+	<td valign="TOP" height="60" rowspan="2" width="100%"><a><img src="https://i.postimg.cc/75dDNrjr/ZonalBus.png" border="0" alt="LogotipoZonal"></a></td>
+	<td valign="TOP" height="20" align="RIGHT"><img src="https://web.archive.org/web/20000619204746im_/http://support.microsoft.com/isapi/gosupport.asp?target=/library/toolbar/images/curve.gif" width="18" height="20" border="0"></td>
+
+<!--Start Global Toolbar Section-->
+<td bgcolor="#000000" height="20" valign="MIDDLE" align="RIGHT" nowrap="" colspan="2">
+  <font color="#FFFFFF" face="Verdana, Arial" size="1">
+  
+  </font></td>
+<!--End Global Toolbar Section-->
+
+</tr>
+<tr>
+	<td valign="TOP" height="40" width="19"><img src="https://web.archive.org/web/20000619204746im_/http://support.microsoft.com/library/images/gifs/homepage/1ptrans.gif" width="19" height="40" border="0"></td>
+	<td valign="TOP" height="40" align="RIGHT" nowrap="" colspan="2"><a target="_top"><img src="https://i.postimg.cc/gncYzD3Q/Logotipo-Digital-Studios.jpg" width="112" height="40" alt="LogoDigitalStudios" border="0"></a></td>
+</tr>
+<!-- Start: Black Line Spacer  -->
+<tr>
+	<td bgcolor="#000000" height="1" nowrap="" colspan="4"></td>
+</tr>
+<!-- End: Black Line Spacer -->
+<tr>
+	<!-- Start: Local menus -->
+	<td bgcolor="#000000" height="20" valign="MIDDLE" nowrap="" colspan="4">
+		<font color="#FFFFFF" face="Verdana, Arial" size="1"><b>
+
+			&nbsp;&nbsp;<a style="color:#FFFFFF;text-decoration:none;" href="/" target="_top" title="Home"><font color="#FFFFFF">Inicio</font></a>&nbsp;&nbsp;<font color="#FFFFFF">|</font>
+
+			&nbsp;&nbsp;<a style="color:#FFFFFF;text-decoration:none;" href="https://www.roblox.com/games/78152937610608/" target="_top" title="Experiencia Roblox"><font color="#FFFFFF">Experiencia Roblox</font></a>&nbsp;&nbsp;<font color="#FFFFFF">|</font>
+
+			&nbsp;&nbsp;<a style="color:#FFFFFF;text-decoration:none;" href="https://github.com/IJustDavi/asistenteverifdsctransbus/blob/main/" target="_top" title="Proyecto Github"><font color="#FFFFFF">Repositorio GitHub</font></a>&nbsp;&nbsp;
+
+		</b></font></td>
+	<!-- End: Local menus -->
+</tr>
+<!-- Start: Black Line Spacer  -->
+<tr>
+	<td bgcolor="#000000" height="1" nowrap="" colspan="4"></td>
+</tr>
+<!-- End: Black Line Spacer -->
+</tbody></table>
+</span>
+<!-- End: ToolBar For down-level browsers-->
+
+<script language="JavaScript">
+<!--// Hide from old browsers
+	var ToolBar_Supported = ToolBar_Supported;
+	if (ToolBar_Supported != null && ToolBar_Supported == true)
+	{
+		TBDownLevelDiv.style.display = "none";
+		drawToolbar();
+	}
+//-->
+</script>
+<!-- End: ToolBar V2.0-->
+<!--TOOLBAR_END-->
+<table border="0" cellpadding="0" cellspacing="0" width="100%">
+	<tbody><tr>
+		<td valign="top">
+			<center>
+			<table border="0" cellpadding="0" cellspacing="0" width="95%">
+				<tbody><tr>
+					<td valign="top">
+						<font face="Verdana, Arial, Helvetica" size="2">
+<!-- ---------- PAGE TEXT BEGINS BELOW HERE ---------- -->
+<table border="0" cellpadding="3" cellspacing="5" width="400">
+	<tbody><tr>
+		<td align="left" valign="top">
+			<img height="33" src="https://web.archive.org/web/20000619204746im_/http://support.microsoft.com/library/images/support/error/pagerror.gif" width="25">
+		</td>
+		<td align="left" valign="middle" width="360">
+			<h3 style="color: black; font: 13pt/15pt verdana">Ya puedes usar el bot de verificación del servidor TransBus</h3>
+		</td>
+	</tr>
+	<tr>
+		<td colspan="2" width="400">
+			<font face="Verdana, Arial, Helvetica" size="1" style="color: black; font: 8pt/11pt verdana">
+			Por razones de seguridad, el sistema se enciende solo cuando es necesario. Pero ya puedes continuar con el proceso.
+			</font>
+		</td>
+	</tr>
+	<tr>
+		<td colspan="2" width="400">
+			<font face="Verdana, Arial, Helvetica" size="1" style="color: black; font: 8pt/11pt verdana">
+			<hr color="#C0C0C0" noshade="">
+			Si tienes dudas e inquietudes, te las recibimos con gusto creando un Ticket en el servidor. Muchas gracias por tu colaboración.
+			
+			<br></font>
+		</td>
+	</tr>
+</tbody></table>
+<!-- ----------- PAGE TEXT ENDS ABOVE HERE ----------- -->
+						</font>
+					</td>
+				</tr>
+			</tbody></table>
+			<p>
+			<table border="0" cellpadding="0" cellspacing="0" width="98%">
+				<tbody><tr>
+					<td valign="top">
+						<font face="Verdana, Arial, Helvetica" size="1">
+<!--COPYRIGHT_START-->
+<a href="https://x.com/IJustDavi">Sistema elaborado por IJustDavi, revisión 25 noviembre 2024.</a><br>
+<!--COPYRIGHT_END-->
+						</font>
+					</td>
+				</tr>
+			</tbody></table>
+			</p></center>
+		</td>
+	</tr>
+</tbody></table>
+
+
+</body></html>
