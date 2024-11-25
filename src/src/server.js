0efa0609157b1dd34c96e8f38ac593a3ee470ea5
IJustDavi <87678581+IJustDavi@users.noreply.github.com>
2024-11-25 11:53:11 -0500

    Inserción index.html

--- src/src/server.js
+++ src/src/server.js
@@ -6,6 +6,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const dataPath = './src/data.json';
 
+// Sirve los archivos estáticos desde la carpeta de activos
+app.use(express.static(path.join(__dirname, 'assets')));
+
 app.use(express.json());
 
 // Endpoint para obtener todos los códigos generados
@@ -13,6 +16,12 @@ app.get('/codes', (req, res) => {
     const data = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
     res.json(data);
 });
+
+// Enviar el archivo index.html cuando los usuarios visiten la raíz ("/")
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'assets', 'index.html'));
+});
+
 
 // Inicia el servidor
 app.listen(PORT, () => {
